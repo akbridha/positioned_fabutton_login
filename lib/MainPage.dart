4d47a6c39a57aaca386a57db8811ca92087f44ad
ridha <akbridha@gmail.com>
2021-09-07 15:06:45 +0800

    commit 3

--- lib/MainPage.dart
+++ lib/MainPage.dart
@@ -12,23 +12,6 @@ class MainPage extends StatelessWidget {
         body: Stack(
       children: <Widget>[
         Positioned(
-          right: -getSmallerDiameter(context) / 3,
-          top: -getSmallerDiameter(context) / 3,
-          child: Container(
-            width: getSmallerDiameter(context),
-            height: getSmallerDiameter(context),
-            decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                gradient: LinearGradient(
-                  colors: [Color(0xda52f600), Color(0xff44C9f7)],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                )),
-            //  Colors.accents,
-          ),
-        ),
-
-        Positioned(
           left: -getSmallerDiameter(context) / 3,
           top: -getSmallerDiameter(context) / 3,
           child: Container(
@@ -53,6 +36,22 @@ class MainPage extends StatelessWidget {
             //  Colors.accents,
           ),
         ),
+        Positioned(
+          right: -getSmallerDiameter(context) / 3,
+          top: -getSmallerDiameter(context) / 3,
+          child: Container(
+            width: getSmallerDiameter(context),
+            height: getSmallerDiameter(context),
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: LinearGradient(
+                  colors: [Color(0xda52f600), Color(0xff44C9f7)],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                )),
+            //  Colors.accents,
+          ),
+        ),
 
         Positioned(
           right: -getSmallerDiameter(context) / 2,
@@ -72,96 +71,115 @@ class MainPage extends StatelessWidget {
           ),
         ),
 
-        Align(
-          alignment: Alignment.center,
-          child: ListView(
-            children: [
-              Container(
+        // Align(
+        //   alignment: Alignment.center,
+        //   child:
+        ListView(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(5)),
+              margin: EdgeInsets.fromLTRB(20, 350, 20, 10),
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 25),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                        icon: Icon(
+                          Icons.email,
+                          color: Color(0xff0ADEF7),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xffF90ED0))),
+                        labelText: "E-mail"),
+                  ),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        icon: Icon(
+                          Icons.vpn_key,
+                          color: Color(0xff0ADEF7),
+                        ),
+                        labelText: "Password"
+                        // focusedBorder: UnderlineInputBorder(
+                        //     borderSide:
+                        //         BorderSide(color: Color(0xffF90ED0)))
+                        ),
+                  )
+                ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Container(
                 decoration: BoxDecoration(
                     color: Colors.white,
                     border: Border.all(color: Colors.grey),
                     borderRadius: BorderRadius.circular(5)),
-                margin: EdgeInsets.fromLTRB(20, 350, 20, 10),
-                padding: EdgeInsets.fromLTRB(10, 0, 10, 25),
-                child: Column(
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(
-                          icon: Icon(
-                            Icons.email,
-                            color: Color(0xff0ADEF7),
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(color: Color(0xffF90ED0))),
-                          labelText: "E-mail"),
-                    ),
-                    TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          icon: Icon(
-                            Icons.vpn_key,
-                            color: Color(0xff0ADEF7),
-                          ),
-                          labelText: "Password"
-                          // focusedBorder: UnderlineInputBorder(
-                          //     borderSide:
-                          //         BorderSide(color: Color(0xffF90ED0)))
-                          ),
-                    )
-                  ],
+                margin: EdgeInsets.fromLTRB(0, 0, 20, 0),
+                child: Text(
+                  "FORGOT PASSWORD",
+                  style: TextStyle(color: Color(0xffFE0460)),
                 ),
               ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: Container(
-                  margin: EdgeInsets.fromLTRB(0, 0, 20, 0),
-                  child: Text(
-                    "FORGOT PASSWORD",
-                    style: TextStyle(color: Color(0xffFE0460)),
-                  ),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.fromLTRB(20, 0, 20, 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.3,
-                      height: 40,
-                      child: Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(6),
-                            gradient: LinearGradient(
-                                begin: Alignment.centerLeft,
-                                end: Alignment.centerRight,
-                                colors: [
-                                  Color(0xda52f600),
-                                  Color(0xff44C9f7)
-                                ])),
-                        child: Material(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.transparent,
-                          child: InkWell(
-                            borderRadius: BorderRadius.circular(20),
-                            splashColor: Colors.cyanAccent,
-                            onTap: () {},
-                            child: Center(
-                              child: Text(
-                                "SIGN IN",
-                                style: TextStyle(color: Colors.white),
-                              ),
-                            ),
+            ),
+
+            //button
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(5)),
+              margin: EdgeInsets.fromLTRB(20, 0, 20, 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    height: 40,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          gradient: LinearGradient(
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                              colors: [Color(0xda52f600), Color(0xff44C9f7)])),
+                      child: InkWell(
+                        borderRadius: BorderRadius.circular(5),
+                        splashColor: Colors.cyanAccent,
+                        onTap: () {},
+                        child: Center(
+                          child: Text(
+                            "SIGN IN",
+                            style: TextStyle(color: Colors.white),
                           ),
                         ),
+
+                        //  Material(
+                        //   borderRadius: BorderRadius.circular(5),
+                        //   color: Colors.transparent,
+                        //   child: InkWell(
+                        //     borderRadius: BorderRadius.circular(5),
+                        //     splashColor: Colors.cyanAccent,
+                        //     onTap: () {},
+                        //     child: Center(
+                        //       child: Text(
+                        //         "SIGN IN",
+                        //         style: TextStyle(color: Colors.white),
+                        //       ),
+                        //     ),
+                        //   ),
                       ),
                     ),
-                  ],
-                ),
-              )
-            ],
-          ),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
+        // ),
 
         // Align(
         //   // alignment: Alignment.center,
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'MainPage.dart';
 
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MainPage(),
     );
   }
